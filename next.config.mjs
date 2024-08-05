4fef2428bac5d2d31280b891d9db92a657585ce3
Mirzamurod <rahimberdiyevmirzamurod@gmail.com>
2024-08-05 17:48:09 +0500

    changed backend url

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   // env: { BACKEND_URL: 'http://localhost:5000/api/' },
-  env: { BACKEND_URL: 'http://206.189.109.20:9090/api' }
+  env: { BACKEND_URL: 'http://206.189.109.20:9090/api/' }
 }
 
 export default nextConfig
