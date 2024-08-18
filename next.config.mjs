a86c0d7e3686dcc2b73ac9a3306cc289a7d31a08
Mirzamurod <rahimberdiyevmirzamurod@gmail.com>
2024-08-18 18:25:19 +0500

    changed next config

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,12 @@ const nextConfig = {
   reactStrictMode: false,
   env: { BACKEND_URL: 'http://localhost:5000/api/' },
   // env: { BACKEND_URL: 'http://206.189.109.20:9090/api/' },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.optimization.splitChunks = false
-    }
-    return config
-  },
+  // webpack: (config, { isServer }) => {
+  //   if (isServer) {
+  //     config.optimization.splitChunks = false
+  //   }
+  //   return config
+  // },
 }
 
 export default withPWA({ dest: 'public' })(nextConfig)
