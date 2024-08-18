2e8d4bf37a0c58da057c963164572689030170ed
Mirzamurod <rahimberdiyevmirzamurod@gmail.com>
2024-08-18 16:03:05 +0500

    changed manifest.json

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,13 @@ import withPWA from 'next-pwa'
 const nextConfig = {
   reactStrictMode: false,
   env: { BACKEND_URL: 'http://localhost:5000/api/' },
-  // env: { BACKEND_URL: 'http://206.189.109.20:9090/api/' }
+  // env: { BACKEND_URL: 'http://206.189.109.20:9090/api/' },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.optimization.splitChunks = false
+    }
+    return config
+  },
 }
 
 export default withPWA({ dest: 'public' })(nextConfig)
